83912e14457f88999d1ad28185908cc1b3a808af
Wang <samuelwangkc@gmail.com>
2018-11-30 11:39:57 -0800

    why missmatch

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,5 +1,9 @@
 
 var game = {
+    questionNum: 0,
+    playerChoice: "",
+    playerCorrect: 0,
+    playerIncorrect: 0,
     trivia: [
         {
             question: "What is the first Pokemon in the Pokedex",
@@ -49,7 +53,13 @@ var game = {
         $("#a2").html(game.trivia[num].answer2);
         $("#a3").html(game.trivia[num].answer3);
         $("#a4").html(game.trivia[num].answer4);
-    }
+
+        $("#a1").attr("choice",game.trivia[num].answer1);
+        $("#a2").attr("choice",game.trivia[num].answer2);
+        $("#a3").attr("choice",game.trivia[num].answer3);
+        $("#a4").attr("choice",game.trivia[num].answer4);
+    },
+    
 }
 
 
@@ -60,5 +70,19 @@ $(document).ready(function () {
 
 $("#start-button").on("click", function () {
     game.initBoard(0);
-    console.log("triggered");
+});
+
+$("body").on("click", ".answer" ,function () {
+    game.playerChoice = $(this).attr("choice");
+    var correctAnswer = game.trivia[game.questionNum].correctAnswer;
+    console.log("selected: "+game.playerChoice);
+    console.log("correct: "+game.trivia[game.questionNum].correctAnswer);
+    if(game.playerChoice === correctAnswer){
+        game.playerCorrect ++;
+    } else {
+        game.playerIncorrect ++;
+    }
+    console.log(`Correct: ${game.playerCorrect} | Incorrect: ${game.playerIncorrect}`);
+    game.questionNum++;
+    game.initBoard(game.questionNum);
 });
